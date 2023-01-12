c4c558656a2a2e9bacd0ae451751726a7bc16c09
Andrea Di Stefano <79949135+galvanizedheart@users.noreply.github.com>
2023-01-13 00:56:31 +0100

    Almost done

--- script/script.js
+++ script/script.js
@@ -1,6 +1,23 @@
+const url = 'http://localhost:8000/';
+
+function article(param) {
+    if (param == null || param == "") {
+        location.href = 'index.html';
+    } else {
+        var settings = {
+            url: url + 'article?p=' + param,
+            method: 'GET',
+            timeout: 0,
+        }
+        $.ajax(settings).done(function(res) {
+            popArticle(res);
+        });
+    }
+}
+
 function blog(sel) {
     var settings = {
-        url: 'http://localhost:8000/blogposts?p=' + sel,
+        url: url + 'blogposts?p=' + sel,
         method: 'GET',
         timeout: 0,
     };
@@ -13,13 +30,42 @@ function blog(sel) {
 
 function projects() {
     var settings = {
-        url: 'http://localhost:8000/projects',
+        url: url + 'projects',
         method: 'GET',
         timeout: 0,
     }
     $.ajax(settings).done(function(res) {
         popProj(res);
     });
+}
+
+function popArticle(res) {
+    console.log(res);
+    const main = document.getElementById("bigpage");
+    if (res[0].ISSOFTWARE == 1) {
+        main.style.backgroundImage = "linear-gradient(to bottom left, darkslategrey, 1%, black)";
+    }
+    let art = document.getElementsByTagName("article")[0];
+
+    let h1 = document.createElement("h1");
+    let p = document.createElement("p");
+    let date = document.createElement("p");
+
+    date.setAttribute("class", "date");
+    date.innerHTML = res[0].DATETAG.substring(0, 10);
+    h1.innerHTML = res[0].TITLE;
+    p.innerHTML = res[0].BODY;
+    art.appendChild(h1);
+    art.appendChild(p);
+    if (res[0].ISSOFTWARE == 1) {
+        const link = document.createElement("button");
+        link.setAttribute("class", "link");
+        link.setAttribute("onclick", "location.href='" + res[0].LINK + "'");
+        link.innerHTML = "Check it out!";
+        art.appendChild(link);
+    }
+    art.appendChild(date);
+
 }
 
 function popProj(res) {
@@ -41,7 +87,7 @@ function popProj(res) {
         art.appendChild(h1);
         art.appendChild(p);
         art.appendChild(date);
-        art.setAttribute("onclick", "location.href='project.html?no=" + res[i].ID + "'");
+        art.setAttribute("onclick", "location.href='article.html?art=" + res[i].ID + "'");
 
         main.appendChild(art);
     }
