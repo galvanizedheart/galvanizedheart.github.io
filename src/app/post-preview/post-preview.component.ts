671171fc1ad1a48c5a02a0f35c0fbfa77127de80
Andrea Di Stefano <andreadistefano.work@gmail.com>
2024-05-01 00:21:36 +0200

    Individual post view functionality

--- src/app/post-preview/post-preview.component.ts
+++ src/app/post-preview/post-preview.component.ts
@@ -1,26 +1,31 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import {Component, Input, input, OnInit} from '@angular/core';
 import { marked } from 'marked';
+import { routes } from '../app.routes';
+import { Router, RouterOutlet } from '@angular/router';
 import  clip  from 'text-clipper';
 import {FileService} from "../file.service";
 
 @Component({
   selector: 'app-post-preview',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, RouterOutlet],
   providers: [FileService],
   templateUrl: './post-preview.component.html',
   styleUrl: './post-preview.component.css',
 })
 export class PostPreviewComponent {
+  
   @Input() file: string;
   body:any;
   title:any;
   subtitle:any;
   data:any;
-  constructor(private http:HttpClient,private fileService:FileService) {
+  
+  constructor(private http:HttpClient,private fileService:FileService,private router:Router) {
     this.file='';
   }
+  
   ngOnInit() {
     const parsed = JSON.parse(JSON.stringify(this.file));
     this.fileService.getFile(parsed.name).subscribe(data => {
@@ -34,5 +39,12 @@ export class PostPreviewComponent {
       const clippedHtml = clip(doc.body.innerHTML, 500, {html: true, maxLines: 10});
       this.body = clippedHtml + '<p style="color:red!important">[...]</p>';
     });
+  }
+  
+  navToPost() {
+    const parsed = JSON.parse(JSON.stringify(this.file));
+    this.router.navigate(
+      ['/post'], 
+      { queryParams: { id: parsed.name } }).then(r => {return r});
   }
 }
--- src/app/post-view/post-view.component.ts
+++ src/app/post-view/post-view.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {Component, Input, input, OnInit} from '@angular/core';
+import { marked } from 'marked';
+import { ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs';
+import {FileService} from "../file.service";
 import {PostPreviewComponent} from "../post-preview/post-preview.component";
 
 @Component({
   selector: 'app-post-view',
   standalone: true,
   imports: [
-    PostPreviewComponent
+    PostPreviewComponent, HttpClientModule
   ],
-  templateUrl: './post-view.component.html',
-  styleUrl: './post-view.component.css'
+  providers: [FileService],
+  templateUrl: '../post-preview/post-preview.component.html',
+  styleUrls: ['../post-preview/post-preview.component.css', './post-view.component.css']
 })
 export class PostViewComponent {
+  
+  fileName:any;
+  body:any;
+  title:any;
+  subtitle:any;
+  data:any;
+  constructor(private http:HttpClient,private fileService:FileService,private route:ActivatedRoute) {
+    this.fileName='';
+  }
 
+  navToPost(){}
+
+  ngOnInit() {
+    this.route.queryParamMap
+      .subscribe(params => {
+        this.fileName = params.get('id');
+      });
+    const parsed = JSON.parse(JSON.stringify(this.fileName));
+    console.log(parsed);
+    this.fileService.getFile(parsed).subscribe(data => {
+      this.data = marked(data);
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(this.data, 'text/html');
+      this.title = doc.querySelector('h1')?.innerHTML;
+      this.subtitle = doc.querySelector('h2')?.innerHTML;
+      doc.querySelector('h1')?.remove();
+      doc.querySelector('h2')?.remove();
+      this.body = doc.body.innerHTML;
+    });
+  }
 }
